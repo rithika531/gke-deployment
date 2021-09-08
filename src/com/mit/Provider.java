f8599a92335350c3e49774c831dee8bb4105a6d4
rithika531 <90042969+rithika531@users.noreply.github.com>
2021-09-08 11:16:52 +0530

    Update Provider.java

--- src/com/mit/Provider.java
+++ src/com/mit/Provider.java
@@ -4,7 +4,7 @@ public interface Provider {
 	
 	String username="postgres";    
 	String pwd="Akbp3p1gEjkgvLlz";  
-	String connURL="jdbc:postgresql://34.66.225.19:5432/postgres"; 
+	String connURL="jdbc:postgresql://127.0.0.1:5432/postgres"; 
 	
 
 } 
