3e7270843e6b435f007944738a5f2f62c4800b1b
rithika531 <90042969+rithika531@users.noreply.github.com>
2021-10-01 14:40:05 +0530

    Update ConnectionProvider.java

--- src/com/mit/ConnectionProvider.java
+++ src/com/mit/ConnectionProvider.java
@@ -1,14 +1,29 @@
 package com.mit;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
-public class ConnectionProvider implements Provider {
+public class ConnectionProvider{
 	
 	static Connection con=null ;
 	
-	public static Connection getCon() {
+	public static Connection getCon() throws IOException {
+		Properties dbProps = new Properties();
+		String path = "/Users/rithika/Desktop/config.properties";
+		try {
+	        InputStream is = new FileInputStream(path);
+	        dbProps.load(is);
+	        
+	        //String url = dbProps.getProperty("connectionURL");
+	        //System.out.println(url); 
+	    } catch (Exception e) {
+	        throw new IOException("Could not read properties file");
+	    }
 		try {
 			Class.forName("org.postgresql.Driver");
-			con=DriverManager.getConnection(connURL, username, pwd);
+			con=DriverManager.getConnection(dbProps.getProperty("connectionURL"), dbProps.getProperty("username"), dbProps.getProperty("password"));
 		}catch(Exception ex) {
 			System.out.println(ex);
 		}
